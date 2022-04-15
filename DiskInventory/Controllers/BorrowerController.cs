a806fee0b36125a3c05e002c2df091ec5fa3a949
JWokersien <JWokersien@SERVERB00>
2022-04-15 14:33:35 -0600

    Add project files.

---
+++ DiskInventory/Controllers/BorrowerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DiskInventory.Models;                                                                                 // add for P2
+
+namespace DiskInventory.Controllers
+{
+    public class BorrowerController : Controller
+    {
+        private disk_inventoryjwContext context { get; set; }                                               // add for P2
+        public BorrowerController(disk_inventoryjwContext ctx)                                              // add for P2
+        {
+            context = ctx;
+        }
+        public IActionResult Index()
+        {
+            List<Borrower> borrowers = context.Borrowers.OrderBy(b => b.Lname).ThenBy(b => b.Fname).ToList();   // add for P2
+            return View(borrowers);                                                                             // updated for P2
+        }
+    }
+}
---
+++ DiskInventory/Models/ViewBorrowerNoLoans2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DiskInventory.Models
+{
+    public partial class ViewBorrowerNoLoans2
+    {
+        public int BorrowerId { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+    }
+}
