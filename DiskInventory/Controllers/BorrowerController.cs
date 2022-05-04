95a020674e1d95fa1983e4b740475e1d00468ad2
JWokersien <JWokersien@SERVERB00>
2022-05-04 16:38:56 -0600

    Changed a, u, & d to use stored procedures.

--- DiskInventory/Controllers/BorrowerController.cs
+++ DiskInventory/Controllers/BorrowerController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DiskInventory.Models;                                                                                 // add for P2
+using Microsoft.EntityFrameworkCore;
 
 namespace DiskInventory.Controllers
 {
@@ -18,6 +19,65 @@ namespace DiskInventory.Controllers
         {
             List<Borrower> borrowers = context.Borrowers.OrderBy(b => b.Lname).ThenBy(b => b.Fname).ToList();   // add for P2
             return View(borrowers);                                                                             // updated for P2
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            return View("Edit", new Borrower());
+        }
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            var borrower = context.Borrowers.Find(id);
+            return View(borrower);
+        }
+        [HttpPost]
+        public IActionResult Edit(Borrower borrower)
+        {
+            if (ModelState.IsValid)
+            {
+                if (borrower.BorrowerId == 0)
+                {
+                    //context.Borrowers.Add(borrower);
+                    context.Database.ExecuteSqlRaw("execute sp_ins_borrower @p0, @p1, @p2",
+                        parameters: new[] { borrower.Fname, borrower.Lname, borrower.PhoneNum });
+                    TempData["message"] = "Borrower added.";
+                }
+
+                else
+                {
+                    //context.Borrowers.Update(borrower);
+                    context.Database.ExecuteSqlRaw("execute sp_upd_borrower @p0, @p1, @p2, @p3",
+                       parameters: new[] { borrower.BorrowerId.ToString(), borrower.Fname, borrower.Lname, borrower.PhoneNum });
+                    TempData["message"] = "Borrower updated.";
+                }
+
+                //context.SaveChanges();
+                return RedirectToAction("Index", "Borrower");
+            }
+            else
+            {
+                ViewBag.Action = (borrower.BorrowerId == 0) ? "Add" : "Edit";
+                return View(borrower);
+            }
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var borrower = context.Borrowers.Find(id);
+            return View(borrower);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Borrower borrower)
+        {
+            //context.Borrowers.Remove(borrower);
+            //context.SaveChanges();
+            context.Database.ExecuteSqlRaw("execute sp_del_borrower @p0",
+                parameters: new[] { borrower.BorrowerId.ToString() });
+            TempData["message"] = "Borrower removed.";
+            return RedirectToAction("Index", "Borrower");
         }
     }
 }
--- DiskInventory/Controllers/DiskController.cs
+++ DiskInventory/Controllers/DiskController.cs
@@ -29,9 +29,64 @@ namespace DiskInventory.Controllers
             ViewBag.DiskTypes = context.DiskTypes.OrderBy(t => t.Description).ToList();
             return View("Edit", new Disk());
         }
-        //public IActionResult Edit()
-        //{
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.Genres = context.Genres.OrderBy(g => g.Description).ToList();
+            ViewBag.Statuses = context.Statuses.OrderBy(s => s.Description).ToList();
+            ViewBag.DiskTypes = context.DiskTypes.OrderBy(dt => dt.Description).ToList();
+            var disk = context.Disks.Find(id);
+            return View(disk);
+        }
+        [HttpPost]
+        public IActionResult Edit(Disk disk)
+        {
+            if (ModelState.IsValid)
+            {
+                if (disk.DiskId == 0)
+                {
+                    //context.Disks.Add(disk);
+                    context.Database.ExecuteSqlRaw("execute sp_ins_disk @p0, @p1, @p2, @p3, @p4",
+                        parameters: new[] { disk.DiskName, disk.ReleaseDate.ToString(), disk.GenreId.ToString(), disk.StatusId.ToString(), disk.DiskTypeId.ToString() });
+                    TempData["message"] = "Disk added.";
+                }
 
-        //}
+                else
+                {
+                    //context.Disks.Update(disk);
+                    context.Database.ExecuteSqlRaw("execute sp_upd_disk @p0, @p1, @p2, @p3, @p4, @p5",
+                        parameters: new[] { disk.DiskId.ToString(), disk.DiskName, disk.ReleaseDate.ToString(), disk.GenreId.ToString(), disk.StatusId.ToString(), disk.DiskTypeId.ToString() });
+                    TempData["message"] = "Disk updated.";
+                }
+
+                //context.SaveChanges();
+                return RedirectToAction("Index", "Disk");
+            }
+            else
+            {
+                ViewBag.Action = (disk.DiskId == 0) ? "Add" : "Edit";
+                ViewBag.Genres = context.Genres.OrderBy(g => g.Description).ToList();
+                ViewBag.Statuses = context.Statuses.OrderBy(s => s.Description).ToList();
+                ViewBag.DiskTypes = context.DiskTypes.OrderBy(dt => dt.Description).ToList();
+                return View(disk);
+            }
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var disk = context.Disks.Find(id);
+            return View(disk);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Disk disk)
+        {
+            //context.Disks.Remove(disk);
+            //context.SaveChanges();
+            context.Database.ExecuteSqlRaw("execute sp_del_disk @p0", parameters: new[] { disk.DiskId.ToString() });
+            TempData["message"] = "Disk removed.";
+            return RedirectToAction("Index", "Disk");
+        }
     }
 }
